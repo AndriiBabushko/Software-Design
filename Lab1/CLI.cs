39d88f971eb1706f73d083b9a891e4a3605eff8a
Andrii Babushko <andriibabushko@gmail.com>
2023-02-10 11:47:30 +0200

    Add entering players` tic tac toe roles

---
+++ Lab1/CLI.cs
@@ -0,0 +1,42 @@
+﻿using TicTacToe;
+
+namespace TicTacToeCLI;
+public class CLI
+{
+    public static char EnterPlayerRole(int PlayerNumber, string CheckRole = "")
+    {
+        char Role;
+
+        switch (CheckRole)
+        {
+            case "X":  Console.WriteLine("Existing roles: O."); break;
+            case "O":  Console.WriteLine("Existing roles: X."); break;
+            default:  Console.WriteLine("Existing roles: X, O."); break;
+        }
+       
+        do
+        {
+            Console.Write($"Player {PlayerNumber} role: ");
+            if(char.TryParse(Console.ReadLine(), out Role) && (CheckRole != Role.ToString()) && (Role == 'X' || Role == 'O'))
+                break;
+            else
+            {
+                Console.Write("Error! Role is entered wrong. Repeat: ");
+            }
+        } while(char.TryParse(Console.ReadLine(), out Role) && (CheckRole == Role.ToString()) && (Role == 'X' || Role == 'O'));
+        
+        return Role;
+    }
+
+
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Let's play Tic Tac Toe!");
+
+        char FirstPlayerRole = EnterPlayerRole(1);
+        char SecondPlayerRole = EnterPlayerRole(2, FirstPlayerRole.ToString());
+
+        Console.WriteLine(FirstPlayerRole);
+        Console.WriteLine(SecondPlayerRole);
+    }
+}
