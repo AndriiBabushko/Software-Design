c41a861733d8959430e67b8e96feeafa557e1590
Andrii Babushko <andriibabushko@gmail.com>
2023-02-15 11:14:56 +0200

    Complete TicTacToe v. 1.0(Features: rating, repeat game)

--- Lab1/TicTacToe/TicTacToeLibrary/Player.cs
+++ Lab1/TicTacToe/TicTacToeLibrary/Player.cs
@@ -2,15 +2,29 @@
 {
     public class Player
     {
+        protected struct Core
+        {
+            public int X;
+            public int O;
+            public int TotalGames;
+
+            public Core()
+            {
+                X = 0;
+                O = 0;
+                TotalGames = 0;
+            }
+        }
+
         protected int Number;
         protected string Role;
-        protected int Core;
+        protected Core Rating;
 
         public Player(int playerNumber, string checkRole = "")
         {
             Number = playerNumber;
             Role = EnterPlayerRole(playerNumber, checkRole);
-            Core = 0;
+            Rating = new Core();
         }
 
         public static string EnterPlayerRole(int PlayerNumber, string CheckRole = "")
@@ -19,9 +33,9 @@
 
             switch (CheckRole)
             {
-                case "X": Console.WriteLine("Existing roles: O."); break;
-                case "O": Console.WriteLine("Existing roles: X."); break;
-                default: Console.WriteLine("Existing roles: X, O."); break;
+                case "X": return "O";
+                case "O": return "X";
+                default: Console.WriteLine("Existing game roles: X, O."); break;
             }
 
             do
@@ -40,6 +54,47 @@
             return Role;
         }
 
+        public void SetRating(string RatingKey)
+        {
+            switch (RatingKey)
+            {
+                case "X":
+                    {
+                        Rating.X++;
+                        Rating.TotalGames++;
+                        break;
+                    }
+                case "O":
+                    {
+                        Rating.O += 1;
+                        Rating.TotalGames++;
+                        break;
+                    }
+                case "TotalGames":
+                    {
+                        Rating.TotalGames++;
+                        break;
+                    } 
+            }
+        }
+
+        public void SetRole(string NewRole)
+        {
+            if (NewRole != "X" && NewRole != "O")
+                return;
+
+            Role = NewRole;
+        }
         public string GetRole() => Role;
+        public double GetRating(string RatingKey)
+        {
+            switch (RatingKey)
+            {
+                case "X": return Rating.X;
+                case "O": return Rating.O;
+                case "TotalGames": return Rating.TotalGames;
+                default: return 0;
+            }
+        }
     }
 }
--- Lab1/TicTacToe/TicTacToeLibrary/TicTacToe.cs
+++ Lab1/TicTacToe/TicTacToeLibrary/TicTacToe.cs
@@ -1,19 +1,20 @@
-﻿using System.Data;
-
-namespace TicTacToeLibrary
+﻿namespace TicTacToeLibrary
 {
     public class TicTacToe
     {
         protected static Player[] Players = new Player[2];
         protected static string[,] GameField = new string[3, 3];
 
-        public static void InitializatePlayers()
+        protected static void InitializatePlayers()
         {
-            Players[0] = new Player(1);
-            Players[1] = new Player(2, Players[0].GetRole());
+            if (Players[0] is null && Players[1] is null)
+            {
+                Players[0] = new Player(1);
+                Players[1] = new Player(2, Players[0].GetRole());
+            }   
         }
 
-        public static void InitializateGameFields()
+        protected static void InitializateGameFields()
         {
             int FieldNumber = 1;
             
@@ -29,49 +30,150 @@ namespace TicTacToeLibrary
 
         public static void StartGame()
         {
+            Console.Clear();
             Console.WriteLine("Welcome to Tic Tac Toe!\n");
 
             InitializatePlayers();
+            Menu();
+        }
+
+        protected static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("\tMenu");
+            Console.WriteLine("1. Play");
+            Console.WriteLine("2. Rating");
+            Console.WriteLine("3. Exit");
+
+            int MenuOption;
+            do
+            {
+                Console.Write($"\nSelect menu option: ");
+                if (int.TryParse(Console.ReadLine(), out MenuOption) && (MenuOption >= 1 && MenuOption <= 3))
+                    break;
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"There is no option with '{MenuOption}' in menu.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\nEnter valid option: ");
+                }
+            } while (!int.TryParse(Console.ReadLine(), out MenuOption) && (MenuOption >= 1 && MenuOption <= 3));
+
+            switch (MenuOption)
+            {
+                case 1:
+                    {
+                        Console.Clear();
+                        Play();
+                        break;
+                    }
+                case 2:
+                    {
+                        Console.Clear();
+                        ShowPlayersRating();
+                        break;
+                    }
+                case 3:
+                    {
+                        return;
+                    }
+            }
+        }
+
+        protected static void Play()
+        {
+            string? PlayAgain;
+
+            do
+            {
+                Game();
+
+                Console.Write("Do you want to play again? (y/n) ");
+                PlayAgain = Console.ReadLine();
+                if (PlayAgain == null || (PlayAgain != "y" && PlayAgain != "n"))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Not 'y' or 'n'.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.Write("\nEnter correct symbol: ");
+                    PlayAgain = Console.ReadLine();
+                }
+
+                if(PlayAgain == "y")
+                {
+                    string TempRole = Players[0].GetRole();
+                    Players[0].SetRole(Players[1].GetRole());
+                    Players[1].SetRole(TempRole);
+                }
+            } while (PlayAgain == "y");
+
+            StartGame();
+        }
+
+        protected static void Game()
+        {
             InitializateGameFields();
+
             bool Playing = true;
             uint PlayerNumber;
 
             if (Players[0].GetRole() == "X")
                 PlayerNumber = 1;
-            else 
+            else
                 PlayerNumber = 2;
 
             Console.Clear();
 
             while (Playing)
             {
-                Console.WriteLine("\tLet's play Tic Tac Toe!\n");
+                Console.WriteLine("Let's play Tic Tac Toe!\n");
                 Console.WriteLine($"Player {PlayerNumber}'s turn. Player's role '{Players[PlayerNumber - 1].GetRole()}'.\nSelect from 1 to 9 the game board.\n");
 
-                OutputGameField();
+                ShowGameField();
 
                 if (CheckWin(Players[0].GetRole()))
                 {
-                    Console.WriteLine("First player win! Congrats!");
+                    Console.WriteLine("\n\n\tCongrats!");
+                    Console.WriteLine("First player won!");
+                    Players[0].SetRating(Players[0].GetRole());
+                    Players[1].SetRating("TotalGames");
                     break;
                 }
                 else if (CheckWin(Players[1].GetRole()))
                 {
-                    Console.WriteLine("Second player win! Congrats!");
+                    Console.WriteLine("\n\n\tCongrats!");
+                    Console.WriteLine("Second player won!");
+                    Players[1].SetRating(Players[1].GetRole());
+                    Players[0].SetRating("TotalGames");
+                    break;
+                }
+                else if (IsNoMoves())
+                {
+                    Console.WriteLine("\n\nDraw!");
+                    Console.WriteLine("No players won!");
+                    Players[0].SetRating("TotalGames");
+                    Players[1].SetRating("TotalGames");
                     break;
                 }
 
                 int Move;
                 do
                 {
-                    Console.Write($"\n\nEnter number: ");
-                    if (int.TryParse(Console.ReadLine(), out Move) && (Move >= 1 && Move <= 9))
-                        break;
-                    else
-                        Console.WriteLine("Error! Enter valid number: ");
-                } while (!int.TryParse(Console.ReadLine(), out Move) && (Move >= 1 && Move <= 9));
-
-                TickGameField(Move, Players[PlayerNumber - 1].GetRole());
+                    do
+                    {
+                        Console.Write($"\n\nEnter number: ");
+                        if (int.TryParse(Console.ReadLine(), out Move) && (Move >= 1 && Move <= 9))
+                            break;
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine($"There is no field with '{Move}' on the field.");
+                            Console.ForegroundColor = ConsoleColor.White;
+                            Console.Write("\nEnter valid field: ");
+                        }
+                    } while (!int.TryParse(Console.ReadLine(), out Move) && (Move >= 1 && Move <= 9));
+                } while (!TickGameField(Move, Players[PlayerNumber - 1].GetRole()));
 
                 switch (PlayerNumber)
                 {
@@ -83,26 +185,122 @@ namespace TicTacToeLibrary
             }
         }
 
-        protected static void TickGameField(int Move, string Role)
+        protected static bool TickGameField(int Move, string Role)
         {
+            bool[] IsTicked = new bool[9];
+            int NumericMove;
+
             switch (Move)
             {
                 // 1-st line
-                case 1: GameField[0, 0] = Role; break;
-                case 2: GameField[0, 1] = Role; break;
-                case 3: GameField[0, 2] = Role; break;
+                case 1: 
+                    { 
+                        if(int.TryParse(GameField[0, 0], out NumericMove))
+                            GameField[0, 0] = Role;
+                        else IsTicked[0] = true;
+
+                        break;
+                    }  
+                case 2:
+                    {
+                        if (int.TryParse(GameField[0, 1], out NumericMove))
+                            GameField[0, 1] = Role;
+                        else IsTicked[1] = true;
+
+                        break;
+                    }
+                case 3:
+                    {
+                        if (int.TryParse(GameField[0, 2], out NumericMove))
+                            GameField[0, 2] = Role;
+                        else IsTicked[2] = true;
+
+                        break;
+                    }
                 // 2-nd line
-                case 4: GameField[1, 0] = Role; break;
-                case 5: GameField[1, 1] = Role; break;
-                case 6: GameField[1, 2] = Role; break;
+                case 4:
+                    {
+                        if (int.TryParse(GameField[1, 0], out NumericMove))
+                            GameField[1, 0] = Role;
+                        else IsTicked[3] = true;
+
+                        break;
+                    }
+                case 5:
+                    {
+                        if (int.TryParse(GameField[1, 1], out NumericMove))
+                            GameField[1, 1] = Role;
+                        else IsTicked[4] = true;
+
+                        break;
+                    }
+                case 6:
+                    {
+                        if (int.TryParse(GameField[1, 2], out NumericMove))
+                            GameField[1, 2] = Role;
+                        else IsTicked[5] = true;
+
+                        break;
+                    }
                 // 3-rd line
-                case 7: GameField[2, 0] = Role; break;
-                case 8: GameField[2, 1] = Role; break;
-                case 9: GameField[2, 2] = Role; break;
+                case 7:
+                    {
+                        if (int.TryParse(GameField[2, 0], out NumericMove))
+                            GameField[2, 0] = Role;
+                        else IsTicked[6] = true;
+
+                        break;
+                    }
+                case 8:
+                    {
+                        if (int.TryParse(GameField[2, 1], out NumericMove))
+                            GameField[2, 1] = Role;
+                        else IsTicked[7] = true;
+
+                        break;
+                    }
+                case 9:
+                    {
+                        if (int.TryParse(GameField[2, 2], out NumericMove))
+                            GameField[2, 2] = Role;
+                        else IsTicked[8] = true;
+
+                        break;
+                    }
             }
+
+            for (int i = 0; i < IsTicked.Length; i++)
+                if (IsTicked[i])
+                {
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.Write($"{i + 1} field is already ticked! Choose another one.");
+                    Console.ForegroundColor = ConsoleColor.White;
+                    return false;
+                }
+                   
+            return true;
         }
 
-        protected static void OutputGameField()
+        protected static bool IsNoMoves()
+        {
+            bool IsNoMoves = true;
+
+            for (int i = 0; i < GameField.GetLength(0); i++)
+                for (int j = 0; j < GameField.GetLength(1); j++)
+                {
+                    int Numeric;
+                    if (int.TryParse(GameField[i, j], out Numeric))
+                    {
+                        IsNoMoves = false;
+                        break;
+                    }
+
+                }
+
+            return IsNoMoves;
+        }
+
+        protected static void ShowGameField()
         {
             for (int i = 0; i < GameField.GetLength(0); i++)
             {
@@ -117,6 +315,30 @@ namespace TicTacToeLibrary
                 if(i != GameField.GetLength(0) - 1)
                     Console.WriteLine("\n---+---+---");
             }
+        }
+
+        protected static void ShowPlayersRating()
+        {
+            Console.WriteLine("\tRating");
+            
+
+            for (int i = 0; i < Players.Length; i++)
+            {
+                double WinRate = 0;
+                if (Players[i].GetRating("TotalGames") != 0)
+                    WinRate = Math.Round((Players[i].GetRating("X") + Players[i].GetRating("Y")) / Players[i].GetRating("TotalGames") * 100, 2);
+
+                Console.WriteLine($"\n\tPlayer {i + 1}");
+                Console.WriteLine($"X wins: {(int)Players[i].GetRating("X")}");
+                Console.WriteLine($"O wins: {(int)Players[i].GetRating("O")}");
+                Console.WriteLine($"Total games: {(int)Players[i].GetRating("TotalGames")}");
+                Console.WriteLine($"Win rate: {WinRate}%");
+            }
+            
+            Console.Write("\nPress any KEY to go back to menu..."); 
+            Console.ReadKey();
+
+            StartGame();
         }
 
         protected static bool CheckWin(string PlayerRole)
